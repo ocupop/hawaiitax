a7ab43ecc650b124fec215480524e734250868cb
Jon Burr <yojonathanwburr@gmail.com>
2016-12-15 10:24:03 -0800

    site revisions, missing images, nav behaviour

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,18 @@ $(document).foundation({
     equalize_on_stack: false
   }
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 125
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});

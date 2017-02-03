819370718774e07c04d7484484cc933e10186a1b
Jon Burr <yojonathanwburr@gmail.com>
2017-02-02 16:47:40 -0800

    press page integration, styles, animations

--- src/js/posts.js
+++ src/js/posts.js
@@ -10,17 +10,34 @@
       success: function(result){
         for(var i=0; i<result.length; i++) {
           var new_post = container.find('.post').first().clone();
-          var post_content = result[i].excerpt;
+          var post_excerpt = result[i].excerpt;
+          post_excerpt = $("<div />").html(post_excerpt).text();
+          var post_content = result[i].content;
           post_content = $("<div />").html(post_content).text();
           new_post.find('.post-title').text(result[i].title).end()
                   .find('.post-date').text(result[i].date).end()
+                  .find('.post-excerpt').append(post_excerpt).end()
                   .find('.post-content').append(post_content).end()
-                  .find('.post-attachment').attr('href', result[i].pdf).end()
-                  .find('.post-link').attr('href', result[i].url);
-          container.append(new_post);        
+                  .find('.post-pdf').attr('href', result[i].pdf);
+                  //.find('.post-link').attr('href', result[i].url);
+          container.append(new_post);     
         }
         container.find('.post').first().remove();
         container.addClass('posts-loaded');
     }});
   }
+  
 })();
+
+$(document).on('ready', function(){
+  $('.posts').on('click', '.post-link', function(){
+    window.console.log('clicked');
+    $('.posts').addClass('single-post-view');
+    $(this).closest('div.post').addClass('active');
+  });
+
+  $('.posts').on('click', '.close-post', function(){
+    $('.posts').removeClass('single-post-view');
+    $(this).closest('div.post').removeClass('active');
+  });
+});

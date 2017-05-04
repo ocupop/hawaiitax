dcf722bc96727a7005988b9f5b85b2f30093569a
Jon Burr <yojonathanwburr@gmail.com>
2017-05-04 12:30:19 -0700

    changes to search

--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,6 @@
+---
+#adding front matter to be able to access press_url
+---
 
 jQuery(function() {
   // Initalize lunr with the fields it will be searching on. I've given title
@@ -5,16 +8,33 @@ jQuery(function() {
   window.idx = lunr(function () {
     this.field('name', { boost: 10 });
     this.field('title');
-    this.field('url');
-    this.field('bio');
+    //this.field('url');
+    //this.field('bio');
   });
 
   // Download the data from the JSON file we generated
-  window.data = $.getJSON("/data/search.json");
+  // window.data = function(){
+  //   var data = 
+  // }
+  window.data = {};
+
+  window.data.local = $.getJSON("/data/search.json");
+  window.data.posts = $.getJSON("{{ site.press_url }}/posts.json");
 
   // Wait for the data to load and add it to lunr
-  window.data.then(function(loaded_data){
+  window.data.local.then(function(loaded_data){
     $.each(loaded_data, function(index, value){
+      //window.console.log('value:', value);
+      window.idx.add(
+        $.extend({ "id": index }, value)
+      );
+    });
+  });
+
+  //Wait for the post data to load and add it to lunr
+  window.data.posts.then(function(loaded_data){
+    $.each(loaded_data, function(index, value){
+      //window.console.log('post value:', value);
       window.idx.add(
         $.extend({ "id": index }, value)
       );
@@ -28,43 +48,76 @@ jQuery(function() {
       var query = $("#search_box").val(); // Get the value for the text field
       window.console.log(query);
       var results = window.idx.search(query); // Get lunr to perform a search
-      display_search_results(results); // Hand the results off to be displayed
+      //display_search_results(results); // Hand the results off to be displayed
   });
 
   function display_search_results(results) {
-    var $search_results = $("#search_results");
-
+    window.console.log(results);
     // Wait for data to load
-    window.data.then(function(loaded_data) {
+    // window.data.local.then(function(loaded_data) {
 
-      // Are there any results?
-      if (results.length) {
-        $search_results.empty(); // Clear any old results
+    //   // Are there any results?
+    //   if (results.length) {
+    //     window.console.log('local results', results);
+    //     $("#search-results-local").empty(); // Clear any old results
 
-        // Iterate over the results
-        results.forEach(function(result) {
-          var item = loaded_data[result.ref];
+    //     // Iterate over the results
+    //     // results.forEach(function(result) {
+    //     //   var item = loaded_data[result.ref];
 
-          // Build a snippet of HTML for this result
-          var appendString = '<li>';
-              appendString += '<h2><a href="' + item.url + '">'+ item.name +'</a></h2>';
-              appendString += '<p><em>'+ item.title +'</em></p>';
-              appendString += '<p>'+ item.bio +'</p>';
-              appendString += '</li><hr>';
+    //     //   if(item){
+    //     //     // Build a snippet of HTML for this result
+    //     //     var appendString = '<li>';
+    //     //         appendString += '<h2><a href="' + item.url + '">'+ item.name +'</a></h2>';
+    //     //         appendString += '<p><em>'+ item.title +'</em></p>';
+    //     //         appendString += '<p>'+ item.content +'</p>';
+    //     //         appendString += '</li><hr>';
 
-          // Add it to the results
-          $search_results.append(appendString);
-          $("html, body").animate({ scrollTop: 0 }, "slow");
-          $("#site-search-results").fadeIn();
-        });
-      } else {
-        $search_results.html('<li>No results found</li>');
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        $("#site-search-results").fadeIn();
-      }
-    });
+    //     //     // Add it to the results
+    //     //     $("#search-results-local").append(appendString);
+    //     //   }
+    //     // });
+    //   } else {
+    //     $("#search-results-local").html('<li>No results found</li>');
+    //   }
+    // });
+
+    // window.data.posts.then(function(loaded_data) {
+
+    //   // Are there any results?
+    //   if (results.length) {
+    //     window.console.log('post results', results)
+    //     $("#search-results-posts").empty(); // Clear any old results
+
+    //     // Iterate over the results
+    //     // results.forEach(function(result) {
+    //     //   var item = loaded_data[result.ref];
+    //     //   window.console.log(loaded_data[result.ref]);
+
+    //     //   if(item){
+    //     //     // Build a snippet of HTML for this result
+
+
+    //     //     var appendString = '<li>';
+    //     //         appendString += '<h2><a href="{{ site.press_url }}' + item.url + '">'+ item.title +'</a></h2>';
+    //     //         appendString += '<p><em>'+ item.date + '</em></p>';
+    //     //         appendString += '</li><hr>';
+
+    //     //     // Add it to the results
+    //     //     $("#search-results-posts").append(appendString);
+    //     //   }
+    //     // });
+    //   } else {
+    //     $("#search-results-posts").html('<li>No results found</li>');
+    //   }
+    // });
+
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $("#site-search-results").fadeIn();
   }
 });
+
+
 
 $(document).ready(function(){
   $('#close-search').on('click', function(){

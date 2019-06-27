4e05490b11527c65dcf88ce0fdd0b304816df185
Jon Burr <yojonathanwburr@gmail.com>
2019-06-27 12:32:43 -0700

    scroll to anchor links

--- src/js/main.js
+++ src/js/main.js
@@ -58,8 +58,6 @@ $(document).ready(function() {
 });
 
 $(document).on("click", 'a[href^="#"]', function(event) {
-  event.preventDefault();
-
   $("html, body").animate(
     {
       scrollTop: $($.attr(this, "href")).offset().top - 130

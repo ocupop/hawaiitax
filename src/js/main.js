68dd8f0d878481fbe1cb7907e5dbab4fb8cd0fe4
Jeff Hamlett <j@ocupop.com>
2022-01-25 09:53:32 -0600

    Update umbrella icons

--- src/js/main.js
+++ src/js/main.js
@@ -52,70 +52,70 @@ $(document).ready(function() {
   }
   postClick();
 
-  function loadPosts() {
-    var post_container = $(".posts");
-    var src_url = post_container.data("src-url");
-    $.ajax({
-      type: "GET",
-      url: src_url,
-      success: function(result) {
-        for (var i = 0; i < result.length; i++) {
-          var new_post = post_container
-            .find(".post")
-            .first()
-            .clone();
-          var featured_image = result[i].featured_image;
-          var post_excerpt = result[i].excerpt;
-          post_excerpt = $("<div />")
-            .html(post_excerpt)
-            .text();
-          var post_content = result[i].content;
-          // post_content = $("<div />").html(post_content).text();
-          new_post
-            .find(".post-title .post-link")
-            .text(result[i].title)
-            .attr("href", "#" + result[i].slug)
-            .end()
-            .find(".post-date")
-            .text(result[i].date)
-            .end()
-            .find(".post-excerpt")
-            .append(post_excerpt)
-            .end()
-            .find(".post-featured-image")
-            .each(function() {
-              if (featured_image) {
-                $(this)
-                  .find("img")
-                  .attr("src", featured_image);
-              } else {
-                $(this).remove();
-              }
-            })
-            .end()
-            .find(".post-content")
-            .append(post_content)
-            .end()
-            .find(".post-pdf")
-            .each(function() {
-              if (result[i].pdf) {
-                $(this).attr("href", result[i].pdf);
-              } else {
-                $(this).remove();
-              }
-            });
-          post_container.append(new_post);
-        }
-        post_container
-          .find(".post")
-          .first()
-          .remove();
-        post_container.addClass("posts-loaded");
-      }
-    });
-  }
+  // function loadPosts() {
+  //   var post_container = $(".posts");
+  //   var src_url = post_container.data("src-url");
+  //   $.ajax({
+  //     type: "GET",
+  //     url: src_url,
+  //     success: function(result) {
+  //       for (var i = 0; i < result.length; i++) {
+  //         var new_post = post_container
+  //           .find(".post")
+  //           .first()
+  //           .clone();
+  //         var featured_image = result[i].featured_image;
+  //         var post_excerpt = result[i].excerpt;
+  //         post_excerpt = $("<div />")
+  //           .html(post_excerpt)
+  //           .text();
+  //         var post_content = result[i].content;
+  //         // post_content = $("<div />").html(post_content).text();
+  //         new_post
+  //           .find(".post-title .post-link")
+  //           .text(result[i].title)
+  //           .attr("href", "#" + result[i].slug)
+  //           .end()
+  //           .find(".post-date")
+  //           .text(result[i].date)
+  //           .end()
+  //           .find(".post-excerpt")
+  //           .append(post_excerpt)
+  //           .end()
+  //           .find(".post-featured-image")
+  //           .each(function() {
+  //             if (featured_image) {
+  //               $(this)
+  //                 .find("img")
+  //                 .attr("src", featured_image);
+  //             } else {
+  //               $(this).remove();
+  //             }
+  //           })
+  //           .end()
+  //           .find(".post-content")
+  //           .append(post_content)
+  //           .end()
+  //           .find(".post-pdf")
+  //           .each(function() {
+  //             if (result[i].pdf) {
+  //               $(this).attr("href", result[i].pdf);
+  //             } else {
+  //               $(this).remove();
+  //             }
+  //           });
+  //         post_container.append(new_post);
+  //       }
+  //       post_container
+  //         .find(".post")
+  //         .first()
+  //         .remove();
+  //       post_container.addClass("posts-loaded");
+  //     }
+  //   });
+  // }
 
-  loadPosts();
+  // loadPosts();
 
   // $('a[href*="#"]:not([href="#"])').click(function() {
   //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
